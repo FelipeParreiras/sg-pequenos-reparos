c3332fcd79617915f7d3fdb41a2e3a26537ffc58
FelipeParreiras <felipe.parreiras04@gmail.com>
2025-06-06 09:56:53 -0300

    feat: implementar gerenciamento de usuários pelo admin

--- codigo/backend/src/main/java/com/sg/reparos/controller/UsuarioController.java
+++ codigo/backend/src/main/java/com/sg/reparos/controller/UsuarioController.java
@@ -1,13 +1,16 @@
 package com.sg.reparos.controller;
 
+import com.sg.reparos.dto.UsuarioAdminDTO;
 import com.sg.reparos.dto.UsuarioUpdateDTO;
 import com.sg.reparos.model.Usuario;
+import com.sg.reparos.model.Usuario.TipoUsuario;
 import com.sg.reparos.service.UsuarioService;
 import com.sg.reparos.repository.UsuarioRepository;
 
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -40,9 +43,15 @@ public class UsuarioController {
                 .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
     }
 
-    @PostMapping
+    @PostMapping("/cadstro")
     public Usuario criarUsuario(@RequestBody @Valid Usuario usuario) {
         return usuarioService.salvarUsuario(usuario);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    public Usuario salvarUsuarioPorAdmin(@RequestBody @Valid UsuarioAdminDTO dto) {
+        return usuarioService.salvarUsuarioPorAdmin(dto);
     }
 
     @PutMapping("/perfil")
@@ -53,8 +62,19 @@ public class UsuarioController {
         return usuarioService.atualizarUsuario(usuario.getId(), dto);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/{id}")
+    public Usuario atualizarUsuarioPorAdmin(@PathVariable Long id, @RequestBody @Valid UsuarioAdminDTO dto) {
+        return usuarioService.atualizarUsuarioPorAdmin(id, dto);
+    }
+
     @DeleteMapping("/{id}")
     public void deletarUsuario(@PathVariable Long id) {
         usuarioService.deletarUsuario(id);
+    }
+
+    @GetMapping("/tipos")
+    public TipoUsuario[] listarTiposUsuarios() {
+        return TipoUsuario.values();
     }
 }
